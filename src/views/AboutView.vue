961786896d721293bdfa12a49ce191934ac4f4c6
Anthony Destenay <anthony.destenay@gmail.com>
2023-08-30 12:12:26 +0200

    Added content in About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>Work In Progress</h1>
+    <h1>À propos</h1>
+    <p><strong>Ce site n'est pas un site officiel.</strong> Il a été créé par <a href="https://thoanny.fr"
+        target="_blank">Thoanny</a> et
+      est hébergé par <a href="https://vercel.com" target="_blank">Vercel</a>.</p>
+    <p>Pour personnaliser l'expérience utilisateur de ce site, certaines données sont enregistrées dans votre navigateur
+      en utilisant la fonctionnalité <a href="https://developer.mozilla.org/fr/docs/Web/API/Window/localStorage"
+        target="_blank">localStorage</a>.</p>
+    <p>Les images proviennent du <a href="https://palia.wiki.gg" target="_blank">Wiki officiel de Palia</a> et sont la
+      propriété de Singularity 6 Corporation. Les données des personnages et objets sont issues du jeu Palia et de son
+      Wiki officiel. Elles sont hébergées par <a href="https://lebusmagique.fr" target="_blank">Le Bus Magique</a> et
+      accessibles via son API.
+    </p>
+    <p>Le code source de ce site est disponible sur <a href="https://github.com/thoanny/palia" target="_blank">Github</a>.
+    </p>
   </div>
 </template>
+<style scoped>
+p {
+  @apply mb-4;
+}
+
+a {
+  @apply underline;
+}
+</style>
