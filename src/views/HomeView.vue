f79ce6657194fe44d90fc777efcae44807dbd312
Anthony Destenay <anthony.destenay@gmail.com>
2023-08-30 10:45:14 +0200

    Fixed price and focus quality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -69,27 +69,49 @@ getCharacters().then(c => {
 									</div>
 									<div class="p-6 pt-0 flex flex-col gap-2">
 										<div class="text-xl font-semibold">{{ wish.item.category.name }}</div>
-										<div class="badge text-green-400" v-if="wish.item.focus">
-											+{{ wish.item.focus }} de concentration
-										</div>
-										<div class="flex justify-start items-center gap-2"
-											v-if="wish.item.priceBase || wish.item.priceQuality">
-											<div class="font-semibold">Prix&nbsp;:</div>
-											<div v-if="wish.item.priceBase" class="flex gap-1 items-center">
-												{{ wish.item.priceBase }}
-												<img src="@/assets/gold.png" class="w-5 h-5" alt="pièce(s) d'or">
+
+										<div class="flex gap-1">
+											<div class="badge text-green-400 gap-1" v-if="wish.item.focus">
+												+{{ wish.item.focus }} de concentration
 											</div>
-											<div v-if="wish.item.priceQuality" class="flex gap-1 items-center">
+											<div class="badge text-green-400 gap-1" v-if="wish.item.focusQuality">
 												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-													fill="currentColor" class="w-5 h-5 text-yellow-500">
+													fill="currentColor" class="w-4 h-4 text-yellow-500">
 													<path fill-rule="evenodd"
 														d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
 														clip-rule="evenodd" />
 												</svg>
-												{{ wish.item.priceQuality }}
-												<img src="@/assets/gold.png" class="w-5 h-5" alt="pièce(s) d'or">
+												+{{ wish.item.focusQuality }} de concentration
 											</div>
 										</div>
+
+										<div class="flex gap-4">
+											<div class="flex justify-start items-center gap-1"
+												v-if="wish.item.priceBase || wish.item.priceQuality">
+												<div class="font-semibold">Prix&nbsp;:</div>
+												<div v-if="wish.item.priceBase" class="flex gap-1 items-center">
+													{{ wish.item.priceBase }}
+													<img src="@/assets/gold.png" class="w-6 h-6" alt="pièce(s) d'or">
+												</div>
+											</div>
+											<div class="flex justify-start items-center gap-1"
+												v-if="wish.item.priceQuality">
+												<div class="font-semibold inline-flex gap-1 items-center">
+													Prix <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+														fill="currentColor" class="w-5 h-5 text-yellow-500">
+														<path fill-rule="evenodd"
+															d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+															clip-rule="evenodd" />
+													</svg>:
+												</div>
+												<div v-if="wish.item.priceQuality" class="flex gap-1 items-center">
+													{{ wish.item.priceQuality }}
+													<img src="@/assets/gold.png" class="w-6 h-6" alt="pièce(s) d'or">
+												</div>
+											</div>
+										</div>
+
+
 									</div>
 								</div>
 								<label class="modal-backdrop" :for="'modal-whish' + wish.id">Fermer</label>
