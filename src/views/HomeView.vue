a39e194466f3ecf1398560f3fbec1fa1f4d6aaf4
Anthony Destenay <anthony.destenay@gmail.com>
2023-08-30 12:16:17 +0200

    Disabled characters more button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -163,7 +163,7 @@ getUserWhises();
 							</div>
 						</div>
 					</div>
-					<button class="btn btn-outline btn-circle btn-lg">
+					<button class="btn btn-outline btn-circle btn-lg mr-2" disabled>
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
 							stroke="currentColor" class="w-8 h-8">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
