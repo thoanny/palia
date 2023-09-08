bbb1a1755500cd2674c9bfdc2b787b9f90050288
Anthony Destenay <anthony.destenay@gmail.com>
2023-09-08 14:42:36 +0200

    Change + for > in character details button

--- src/views/CharactersListView.vue
+++ src/views/CharactersListView.vue
@@ -109,7 +109,7 @@ function showAllCharacters() {
                         class="hidden sm:flex btn btn-outline btn-circle btn-lg mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-8 h-8">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                         </svg>
                     </RouterLink>
                 </div>
