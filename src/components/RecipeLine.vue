1b8936671d9b64dfa713418b097f82e900ef8d4f
Anthony Destenay <anthony.destenay@gmail.com>
2023-09-09 12:07:16 +0200

    Fixed recipe craft time condition

--- src/components/RecipeLine.vue
+++ src/components/RecipeLine.vue
@@ -22,7 +22,7 @@ function formatTime(seconds) {
             <ItemModal :item="recipe.workshop" :cls="'workshop'" />
         </div>
         <div class="w-8 flex justify-center">
-            <div class="tooltip" :data-tip="formatTime(recipe.craftTime)" v-if="recipe.craftTime">
+            <div class="tooltip" :data-tip="formatTime(recipe.craftTime)" v-if="recipe.craftTime > 0">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-accent">
                     <path fill-rule="evenodd"
                         d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z"
