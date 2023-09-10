f729eeeb956c5a314cb5e77e0cde48368794f338
Anthony Destenay <anthony.destenay@gmail.com>
2023-09-10 09:08:25 +0200

    Cleaned up router

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 import CharactersList from '../views/CharactersListView.vue';
-// TODO : supprimer component: HomeView
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: HomeView,
-      children: [
-        {
-          path: '',
-          name: 'Characters',
-          component: CharactersList,
-        },
-        {
-          path: 'characters/:slug',
-          name: 'Character',
-          props: true,
-          component: () => import('../views/CharacterView.vue')
-        }
-      ]
+      name: 'Characters',
+      component: CharactersList,
     },
     {
-      path: '/items',
+      path: '/personnages/:slug',
+      name: 'Character',
+      props: true,
+      component: () => import('../views/CharacterView.vue')
+    },
+    {
+      path: '/objets',
       name: 'Items',
       component: () => import('../views/ItemsListView.vue')
     },
     {
-      path: '/recipes',
-      children: [
-        {
-          path: '',
-          name: 'Recipes',
-          component: () => import('../views/RecipesListView.vue')
-        }
-      ]
+      path: '/recettes',
+      name: 'Recipes',
+      component: () => import('../views/RecipesListView.vue')
     },
     {
-      path: '/about',
+      path: '/a-propos',
       name: 'About',
       component: () => import('../views/AboutView.vue')
     }
   ],
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior() {
     return { top: 0 }
   },
 })
