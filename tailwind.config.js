6e48e4855d7a2d1c31ebf2686570f9ccc25fcb79
Anthony Destenay <anthony.destenay@gmail.com>
2024-12-08 15:21:40 +0100

    Fixed DaisyUI import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import daisyui from 'daisyui';
+export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js}",
@@ -26,6 +27,6 @@ module.exports = {
       },
     ],
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 }
 
